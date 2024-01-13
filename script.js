1d6cbb6c2a8aaf794bd230d345b54d8768535197
DanielMakai <danielmakai92@gmail.com>
2024-01-13 04:23:20 +0300

    fixed navbar

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ function myFunction() {
       x.className = "navbar";
     }
   }
+
+  // maitaining fixed navigation bar
+  let navbar = document.querySelector('.navbar');
+
+  let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  window.addEventListener('scroll', function(){
+    let newScrollPosition = window.pageXOffset || this.document.documentElement.scrollTop;
+
+    if(newScrollPosition < scrollPosition){
+      navbar.classList.add('fixed');
+
+    } else{
+      navbar.classList.remove('fixed');
+
+      
+    }
+    scrollPosition = newScrollPosition;
+  });
+
+  // document.querySelector('#contact').addEventListener('click', function(e){
+  //   e.preventDefault();
+
+  //   document.getElementById('contact').scrollIntoView({
+  //     behavior: 'smooth'
+  //   });
+  // });
+
+
